e68aef04fbce8bfd5da20cd906f2678ef7e7c13f
Juan Jose <juanjose@MacBook-Pro-de-Juan.local>
2022-08-16 20:03:30 +0100

    [JJA-9100] Added shared and primeng module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimengModule } from './primeng/primeng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    PrimengModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
